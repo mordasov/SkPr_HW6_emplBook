97d278dd7f8d297ccfe55dfcbad7005ebe8203b2
mss88 <mss88@list.ru>
2024-11-06 13:34:40 +0300

    Initial commit

---
+++ src/main/java/skpro/hw6/emplBook/EmplBookApplication.java
@@ -0,0 +1,13 @@
+package skpro.hw6.emplBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplBookApplication.class, args);
+	}
+
+}
